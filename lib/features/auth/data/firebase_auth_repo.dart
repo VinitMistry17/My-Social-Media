db43d7713ec318bdc7cb47e9514d5f257ae9751e
Vinit Mistry <vinitmistry005@gmail.com>
2025-08-19 13:09:28 +0530

    slot 3 , firebase authentication backend done

--- lib/features/auth/data/firebase_auth_repo.dart
+++ lib/features/auth/data/firebase_auth_repo.dart
@@ -1,26 +1,85 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:my_social_media/features/auth/domain/repos/auth_repo.dart';
 
 import '../domain/entities/app_user.dart';
 
 class FirebaseAuthRepo implements AuthRepo{
+
+  //FirebaseAuth's single instance, used for login/logout/current user
+  //FirebaseAuth = Firebase ka ready-made service hai jo login/logout ka kaam karta hai.
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
   @override
-  Future<AppUser?> signInWithEmailAndPassword(String email, String password) {
-    // TODO: implement signInWithEmailAndPassword
-    throw UnimplementedError();
+  Future<AppUser?> signInWithEmailAndPassword(String email, String password) async {
+    try {
+      //attempt login
+      //UserCredential = a package with user info for ex. email, name, uid etc..
+      UserCredential userCredential = await firebaseAuth
+          .signInWithEmailAndPassword(
+          email: email,
+          password: password
+      );
+
+      //create user
+      AppUser user = AppUser(
+        uid: userCredential.user!.uid,
+        // ! = trust me user is logged in and that's why its not a null
+        email: email,
+        name: '',
+      );
+
+      //return user
+      return user;
+    }
+    //catch errors
+    catch (e) {
+      throw Exception("login failed: $e");
+    }
   }
   @override
-  Future<AppUser?> signUpWithEmailAndPassword(String name, String email, String password) {
-    // TODO: implement signUpWithEmailAndPassword
-    throw UnimplementedError();
+  Future<AppUser?> signUpWithEmailAndPassword(String name, String email, String password) async {
+    try {
+      //attempt signup
+      //UserCredential = a package with user info for ex. email, name, uid etc..
+      UserCredential userCredential = await firebaseAuth
+          .createUserWithEmailAndPassword(
+          email: email,
+          password: password
+      );
+
+      //create user
+      AppUser user = AppUser(
+        uid: userCredential.user!.uid,
+        // ! = trust me user is logged in and that's why its not a null
+        email: email,
+        name: name,
+      );
+      //return user
+      return user;
+    }
+    //catch errors
+    catch (e) {
+      throw Exception("signup failed: $e");
+    }
   }
   @override
-  Future<void> signOut() {
-    // TODO: implement signOut
-    throw UnimplementedError();
+  Future<void> signOut() async {
+    await firebaseAuth.signOut();
   }
   @override
-  Future<AppUser?> getCurrentUser() {
-    // TODO: implement getCurrentUser
-    throw UnimplementedError();
+  Future<AppUser?> getCurrentUser() async {
+    //get current logged in user from firebase
+    final firebaseUser = firebaseAuth.currentUser;
+
+    //if no user logged in
+    if (firebaseUser == null) {
+      return null;
+    }
+    //if user logged in
+    return AppUser(
+      uid: firebaseUser.uid,
+      email: firebaseUser.email!,
+      name: '',
+    );
   }
 }
